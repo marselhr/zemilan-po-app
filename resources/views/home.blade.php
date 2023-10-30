457dae2f7c008f8a90b37ce248e240dc289e40b4
Lukman <103345757+LukmanHakim2204@users.noreply.github.com>
2023-10-30 22:52:23 +0700

    Menambahkan Menu Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+<section class="py-lg-16 py-8">
+    <!-- container -->
+   <div class="container">
+      <!-- row -->
+     <div class="row align-items-center">
+        <!-- col -->
+       <div class="col-lg-6 mb-6 mb-lg-0">
+         <div class="">
+            <!-- heading -->
+           <h5 class="text-dark mb-4"><i class="fe fe-check icon-xxs icon-shape bg-light-success text-success rounded-circle me-2"></i>Zemilan Keripik Baso Goreng</h5>
+              <!-- heading -->
+           <h1 class="display-3 fw-bold mb-3">Zemilan Keripik Baso Goreng</h1>
+            <!-- para -->
+           <p class="pe-lg-10 mb-5">Start, switch, or advance your career with more than 5,000 courses, Professional
+             Certificates, and degrees from world-class universities and companies.</p>
+              <!-- btn -->
+           <a href="#" class="btn btn-primary">Join Free Now</a>
+           <a href="https://www.youtube.com/watch?v=JRzWRZahOVU" class="glightbox fs-4 text-inherit ms-3"><img src="../assets/images/svg/play-btn.svg" alt="" class="me-2">Watch Demo</a>
+         </div>
+       </div>
+        <!-- col -->
+       <div class="col-lg-6 d-flex justify-content-center">
+          <!-- images -->
+        <div class="position-relative text-center">
+         <img src="{{asset('assets/images/background/coba1.JPG')}}" alt="" class="rounded-circle" style="max-width: 450px; height: auto; margin-top: 35px;">
         </div>
-    </div>
-</div>
+       </div>
+     </div>
+   </div>
+ </section>
 @endsection
