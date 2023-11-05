adebb3599115761434ef57ce02b4e7fe90680160
marsel945 <rewoherson519@gmail.com>
2023-11-05 21:21:57 +0800

    style(update user profile picture)

--- resources/views/admin/pages/manajemen-user/show-detail/edit.blade.php
+++ resources/views/admin/pages/manajemen-user/show-detail/edit.blade.php
@@ -12,40 +12,50 @@
         <div class="card-body">
             <div>
                 <!-- Form -->
-                <form class="row gx-3 needs-validation" novalidate action="{{ route('admin.user.update', $user) }}"
-                    method="post">
+                <form class="" action="{{ route('admin.user.update', $user) }}" method="post">
                     @csrf
-                    <div class="col-12">
-                        <label for="avatar" class="form-label">Avatar</label>
-                        <input type="file" name="avatar" src="{{ asset('images/' . $user->avatar) }}">
+                    <div class=" row d-none avatar-xxl col-12 mx-auto mb-4" id="inputFile">
+                        <input type="file" name="avatar">
                     </div>
-                    <!-- First name -->
-                    <div class="mb-3 col-12 col-md-6">
-                        <label class="form-label" for="fname">First Name</label>
-                        <input type="text" id="fname" class="form-control" placeholder="First Name"
-                            value="{{ $user->first_name }}" required name="first_name" />
-                        <div class="invalid-feedback">Please enter first name.</div>
+                    <div id="avatarWrapper"
+                        class="d-flex flex-column align-items-center justify-content-center  mb-4 mb-lg-0">
+                        <img src="{{ asset('images/' . $user->avatar) }}" id="img-uploaded"
+                            class="avatar-xxl rounded-circle" alt="avatar" />
+                        <div class="my-3 mx-1">
+                            <button type="button" id="show-input" class="btn btn-outline-secondary btn-sm">Ubah
+                                Avatar</button>
+                        </div>
                     </div>
-                    <!-- Last name -->
-                    <div class="mb-3 col-12 col-md-6">
-                        <label class="form-label" for="lname">Last Name</label>
-                        <input type="text" id="lname"
-                            class="form-control {{ $errors->has('last_name') ? 'invalid' : '' }}" placeholder="Last Name"
-                            value="{{ $user->last_name }}" name="last_name" />
-                        @error('last_name')
-                            <div class="invalid-feedback">{{ $error->last_name }}</div>
-                        @enderror
-                    </div>
-                    <!-- Phone -->
-                    <div class="mb-3 col-12 col-md-6">
-                        <label class="form-label" for="phone">Phone</label>
-                        <input type="text" id="phone" class="form-control" placeholder="Phone" name="phone_number"
-                            value="{{ $user->phone_number }}" required />
-                        <div class="invalid-feedback">Please enter phone number.</div>
-                    </div>
-                    <div class="col-12">
-                        <!-- Button -->
-                        <button class="btn btn-primary" type="submit">Update Profile</button>
+
+                    <div class="row gx-3">
+                        <!-- First name -->
+                        <div class="mb-3 col-12 col-md-6">
+                            <label class="form-label" for="fname">First Name</label>
+                            <input type="text" id="fname" class="form-control" placeholder="First Name"
+                                value="{{ $user->first_name }}" required name="first_name" />
+                            <div class="invalid-feedback">Please enter first name.</div>
+                        </div>
+                        <!-- Last name -->
+                        <div class="mb-3 col-12 col-md-6">
+                            <label class="form-label" for="lname">Last Name</label>
+                            <input type="text" id="lname"
+                                class="form-control {{ $errors->has('last_name') ? 'invalid' : '' }}"
+                                placeholder="Last Name" value="{{ $user->last_name }}" name="last_name" />
+                            @error('last_name')
+                                <div class="invalid-feedback">{{ $error->last_name }}</div>
+                            @enderror
+                        </div>
+                        <!-- Phone -->
+                        <div class="mb-3 col-12 col-md-6">
+                            <label class="form-label" for="phone">Phone</label>
+                            <input type="text" id="phone" class="form-control" placeholder="Phone"
+                                name="phone_number" value="{{ $user->phone_number }}" required />
+                            <div class="invalid-feedback">Please enter phone number.</div>
+                        </div>
+                        <div class="col-12 flex justify-content-end">
+                            <!-- Button -->
+                            <button class="btn btn-primary" type="submit">Simpan</button>
+                        </div>
                     </div>
                 </form>
             </div>
@@ -54,7 +64,23 @@
 
     <script src="https://unpkg.com/filepond@^4/dist/filepond.js"></script>
     <script src="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.js"></script>
+    <script src="https://unpkg.com/filepond-plugin-image-exif-orientation/dist/filepond-plugin-image-exif-orientation.js">
+    </script>
+    <script src="https://unpkg.com/filepond-plugin-file-validate-type/dist/filepond-plugin-file-validate-type.js"></script>
+    <script src="https://unpkg.com/filepond-plugin-image-edit/dist/filepond-plugin-image-edit.js"></script>
+    <script src="https://unpkg.com/filepond-plugin-image-crop/dist/filepond-plugin-image-crop.js"></script>
+    <script src="https://unpkg.com/filepond-plugin-image-resize/dist/filepond-plugin-image-resize.js"></script>
+    <script src="https://unpkg.com/filepond-plugin-image-transform/dist/filepond-plugin-image-transform.js"></script>
     <script>
+        const btnShow = document.querySelector('#show-input')
+        const inputFile = document.querySelector('#inputFile')
+        const avatarWrapper = document.querySelector('#avatarWrapper')
+        const inputElement = document.querySelector('input[name="avatar"]');
+
+        btnShow.addEventListener('click', function() {
+            inputFile.classList.remove('d-none');
+            avatarWrapper.classList.add('d-none')
+        })
         FilePond.setOptions({
             server: {
                 process: "{{ route('admin.user.avatar.upload', $user) }}",
@@ -66,11 +92,28 @@
         });
 
         // Create the FilePond instance
-        const inputElement = document.querySelector('input[name="avatar"]');
+
         FilePond.registerPlugin(
+            FilePondPluginFileValidateType,
+            FilePondPluginImageExifOrientation,
             FilePondPluginImagePreview,
-        )
+            FilePondPluginImageCrop,
+            FilePondPluginImageResize,
+            FilePondPluginImageTransform,
+            FilePondPluginImageEdit
+        );
         // Create a FilePond instance
-        const pond = FilePond.create(inputElement)
+        const pond = FilePond.create(inputElement, {
+            labelIdle: `Drag & Drop your picture or <span class="filepond--label-action">Browse</span>`,
+            imagePreviewHeight: 170,
+            imageCropAspectRatio: '1:1',
+            imageResizeTargetWidth: 200,
+            imageResizeTargetHeight: 200,
+            stylePanelLayout: 'compact circle',
+            styleLoadIndicatorPosition: 'center bottom',
+            styleProgressIndicatorPosition: 'right bottom',
+            styleButtonRemoveItemPosition: 'left bottom',
+            styleButtonProcessItemPosition: 'right bottom',
+        })
     </script>
 @endsection
