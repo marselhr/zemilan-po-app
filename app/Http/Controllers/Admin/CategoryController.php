65cd28e76be547bc1db21d533bf45618ef5c4ce2
Lukman <103345757+LukmanHakim2204@users.noreply.github.com>
2023-11-07 11:36:23 +0700

    update : fix category

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -12,7 +12,7 @@ class CategoryController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     ** @return \Illuminate\Http\Response
      */
     public function index()
     {
