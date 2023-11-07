b1072a6f94704ed45adc26984ccc45d4204a3ae8
Lukman <103345757+LukmanHakim2204@users.noreply.github.com>
2023-11-07 11:34:35 +0700

    update : fix view Catalog Produk

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -1,14 +1,14 @@
-@extends('layouts.app') {{-- Use your guest layout --}}
+@extends('layouts.app')
 
 @section('content')
 <div class="container">
-    <h4 class="mt-3">Product Catalog</h4>
     <div class="py-4">
+        <h4>Product Catalog</h4>
         <div class="row row-cols-1 row-cols-md-4">
             @foreach($products as $product)
             <div class="col">
                 <!-- Medium-sized Card -->
-                <div class="card" style="width: 18rem; margin: 0.5rem;">
+                <div class="card" style="width: 18rem; margin-right:1px;">
                     <img src="{{ asset('storage/' . $product->image) }}" alt="" class="card-img-top">
                     <!-- Card Body -->
                     <div class="card-body">
@@ -20,13 +20,14 @@
                     </div>
                     <!-- Card Footer -->
                     <div class="card-footer">
-                        <div class="row align-items-center">
-                            <div class="col">
-                                <h5 class="mb-0">{{ $product->price }}</h5>
-                            </div>
-                            <div class="col-auto">
-                                <a href="#" class="text-inherit">
-                                    <i class="fe fe-shopping-cart text-primary align-middle me-2"></i>Get Cart
+                        <div class="d-flex justify-content-between align-items-center">
+                            <h5 class="mb-0">Rp. {{ $product->price }}</h5>
+                            <div class="d-flex gap-2">
+                                <a href="#" class="btn btn-primary" onclick="addToCart({{ $product->id }})">
+                                    <i class="fe fe-shopping-cart text-white align-middle"></i>
+                                </a>
+                                <a href="#" class="btn btn-success ml-2" onclick="checkoutNow({{ $product->id }})">
+                                    <i class="fe fe-check-circle text-white align-middle"></i> Beli
                                 </a>
                             </div>
                         </div>
@@ -37,4 +38,16 @@
         </div>
     </div>
 </div>
+
+<script>
+    function addToCart(productId) {
+        // Implement your logic to add the product to the cart here
+        console.log('Added product with ID: ' + productId + ' to the cart');
+    }
+
+    function checkoutNow(productId) {
+        // Implement your logic to initiate the checkout process here
+        console.log('Initiating checkout for product with ID: ' + productId);
+    }
+</script>
 @endsection
