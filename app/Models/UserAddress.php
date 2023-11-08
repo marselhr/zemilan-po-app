eff04448877a02ad2ae0175ec748ebbf2740dce8
marsel945 <rewoherson519@gmail.com>
2023-11-08 16:36:35 +0800

    Fix bug alamat

--- app/Models/UserAddress.php
+++ app/Models/UserAddress.php
@@ -10,6 +10,7 @@ class UserAddress extends Model
     use HasFactory;
 
 
+    protected $guarded = ['id'];
     public function user()
     {
         return $this->belongsTo(User::class, 'user_id', 'id');
