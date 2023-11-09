9aa3758883f090720e716015136218a9ca680aec
marsel945 <rewoherson519@gmail.com>
2023-11-09 15:27:01 +0800

    update meta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="keywords" content="">
-    <meta name="author" content="Codescandy">
+    <meta name="author" content="Group-3-capstone">
 
     <script>
         // Render blocking JS:
