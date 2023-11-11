61adc4597bc0e44dbba9ede888fb264a855e7ff1
marsel945 <rewoherson519@gmail.com>
2023-11-11 23:21:13 +0800

    fix request validation

--- app/Http/Requests/CreateCouponRequest.php
+++ app/Http/Requests/CreateCouponRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Carbon\Carbon;
 use Illuminate\Foundation\Http\FormRequest;
 
 class CreateCouponRequest extends FormRequest
@@ -24,7 +25,21 @@ class CreateCouponRequest extends FormRequest
         return [
             'code' => 'required|unique:coupons,code',
             'type' => 'required|in:fixed,percent',
-            'value' => 'required|numeric'
+            'value' => 'required|numeric',
+            'max_uses_user' => 'required|numeric',
+            'max_uses' => 'required|numeric',
+            'start_date' => [
+                'required',
+                'date',
+                'date_format:Y-m-d H:i',
+                'after_or_equal:' . Carbon::today()->format('Y-m-d H:i'),
+            ],
+            'expiration_date' => [
+                'required',
+                'date',
+                'date_format:Y-m-d H:i',
+                'after_or_equal:start_date',
+            ],
         ];
     }
 }
