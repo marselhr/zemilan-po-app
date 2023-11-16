d02c53507852c7b6fb16f460bc789df530e59359
Lukman <103345757+LukmanHakim2204@users.noreply.github.com>
2023-11-16 21:23:01 +0700

    add : management controller

---
+++ app/Http/Controllers/Admin/ManagemenPesananController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Order;
+use App\Models\OrderItem;
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class ManagemenPesananController extends Controller
+{
+    public function index()
+    {
+        $user = User::all();
+        $management_pesanan = Order::all();
+        return view('admin.pages.managemen-pesanan.index', compact('management_pesanan', 'user'));
+    }
+    public function show($management_pesanan)
+    {
+        $product = Product::all();
+        $order_items = OrderItem::all();
+        $management_pesanan = Order::findOrFail($management_pesanan);
+        $user = $management_pesanan->user;
+        return view('admin.pages.managemen-pesanan.detail', compact('management_pesanan', 'user', 'order_items', 'product'));
+    }
+}
