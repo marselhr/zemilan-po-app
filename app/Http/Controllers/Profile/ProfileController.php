c72106d8541cc2a500595c164808876945965645
umarfakhr4y <umarfakhr4y@gmail.com>
2023-11-17 20:31:20 +0700

    add: delete old profile pict after changing new profile pict

--- app/Http/Controllers/Profile/ProfileController.php
+++ app/Http/Controllers/Profile/ProfileController.php
@@ -18,26 +18,35 @@ class ProfileController extends Controller
 {
     $user = auth()->user();
 
-
     $request->validate([
         'namaAwal' => 'required|string|max:255',
         'namaAkhir' => 'required|string|max:255',
         'no_telp' => 'required|string|max:255',
         'image' => 'image|mimes:jpeg,png,jpg,gif|max:5000', // Validasi untuk foto profil
     ]);
+
+    // Hapus gambar profil lama jika ada
+    if ($user->avatar) {
+        $oldImagePath = public_path('profile-pictures/') . $user->avatar;
+        if (file_exists($oldImagePath)) {
+            unlink($oldImagePath);
+        }
+    }
+
     // Update data pengguna
     $user->update([
         'first_name' => $request->input('namaAwal'),
         'last_name' => $request->input('namaAkhir'),
         'phone_number' => $request->input('no_telp'),
     ]);
+
     // Cek apakah ada unggahan foto profil
     if ($request->hasFile('image')) {
         $image = $request->file('image');
         $imageName = time() . '.' . $image->getClientOriginalExtension();
         $image->move(public_path('profile-pictures'), $imageName);
 
-        // Simpan nama file ke dalam kolom profile_picture di tabel users
+        // Simpan nama file ke dalam kolom avatar di tabel users
         $user->avatar = $imageName;
         $user->save();
     }
@@ -46,6 +55,7 @@ class ProfileController extends Controller
 
     return redirect()->route('mainprofile')->with('success', 'Profile updated successfully.');
 }
+
 
 
 }
