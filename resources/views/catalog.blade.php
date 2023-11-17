c617d60652e3e91806d3ef0adc1b46eff6bf8b34
marsel945 <rewoherson519@gmail.com>
2023-11-17 22:03:17 +0800

    fix error on order

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @push('customJs')
-
     <!-- Add to cart -->
     <script type="text/javascript">
         $(document).on('click', '.add_to_cart', function(e) {
@@ -37,58 +36,61 @@
 @endpush
 
 @section('content')
-<section>
-    <div class="container">
-        <div class="row mt-4 justify-content-center">
-            <h2>Produk Favorit</h2>
-            <div class="d-flex flex-wrap col-10 justify-content-center">
-                @foreach ($products as $product)
-                    <!-- Medium-sized Card -->
-                    <div href="{{ route('detail', ['id' => $product->id]) }}" class="col-10 col-md-5 col-lg-3 p-3 ">
-                        <div class="card">
-                            <a href="{{ route('detail', ['id' => $product->id]) }}">
-                                <img src="{{ $product->image != null ? asset('storage/' . $product->image) : 'https://source.unsplash.com/480x480?food' }}" alt="" class="card-img-top">
-                                <!-- Card content goes here -->
-                            </a>
-                            <!-- Card Body -->
-                            <div class="card-body">
-                                <div class="d-flex justify-content-between align-items-center mb-1">
-                                    <a href="#" class="fs-5"><i class="fe fe-heart align-middle"></i></a>
+    <section>
+        <div class="container">
+            <div class="row mt-4 justify-content-center">
+                <h2>Produk Favorit</h2>
+                <div class="d-flex flex-wrap col-10 justify-content-center">
+                    @foreach ($products as $product)
+                        <!-- Medium-sized Card -->
+                        <div href="{{ route('detail', ['id' => $product->id]) }}" class="col-10 col-md-5 col-lg-3 p-3 ">
+                            <div class="card">
+                                <a href="{{ route('detail', ['id' => $product->id]) }}">
+                                    <img src="{{ $product->image != null ? asset('storage/' . $product->image) : 'https://source.unsplash.com/480x480?food' }}"
+                                        alt="" class="card-img-top">
+                                    <!-- Card content goes here -->
+                                </a>
+                                <!-- Card Body -->
+                                <div class="card-body">
+                                    <div class="d-flex justify-content-between align-items-center mb-1">
+                                        <a href="#" class="fs-5"><i class="fe fe-heart align-middle"></i></a>
+                                    </div>
+                                    <a href="{{ route('detail', ['id' => $product->id]) }}">
+                                        <h4 class="mb-2 text-truncate">{{ $product->name }}</h4>
+                                    </a>
+                                    <small>Stock: {{ $product->stock }}</small>
                                 </div>
-                                <h4 class="mb-2 text-truncate">{{ $product->name }}</h4>
-                                <small>Stock: {{ $product->stock }}</small>
-                            </div>
-                            <!-- Card Footer -->
-                            <div class="card-footer">
-                                <div class="d-flex flex-wrap justify-content-between align-items-center">
-                                    <h5 class="mb-0">Rp. {{ $product->price }}</h5>
-                                    <div class="d-flex gap-2">
-                                        <div>
-                                            <button type="button" data-product-id="{{ $product->id }}"
-                                                data-quantity="1" class="btn btn-primary btn-sm add_to_cart"
-                                                id="add_to_cart{{ $product->id }}">
-                                                <i class="fe fe-shopping-cart text-white align-middle"></i>
-                                            </button>
-                                        </div>
-
-
-                                        <form action="{{ route('order.store', $product) }}" method="post">
+                                <!-- Card Footer -->
+                                <div class="card-footer">
+                                    <div class="d-flex flex-wrap justify-content-between align-items-center">
+                                        <h5 class="mb-0">Rp. {{ $product->price }}</h5>
+                                        <div class="d-flex gap-2">
                                             <div>
-                                                @csrf
-                                                <button type="submit" class="btn btn-success btn-sm ml-1"
-                                                    onclick="checkoutNow({{ $product->id }})">
-                                                    Beli
+                                                <button type="button" data-product-id="{{ $product->id }}"
+                                                    data-quantity="1" class="btn btn-primary btn-sm add_to_cart"
+                                                    id="add_to_cart{{ $product->id }}">
+                                                    <i class="fe fe-shopping-cart text-white align-middle"></i>
                                                 </button>
                                             </div>
-                                        </form>
+
+
+                                            <form action="{{ route('order.store', $product) }}" method="post">
+                                                <div>
+                                                    @csrf
+                                                    <button type="submit" class="btn btn-success btn-sm ml-1"
+                                                        onclick="checkoutNow({{ $product->id }})">
+                                                        Beli
+                                                    </button>
+                                                </div>
+                                            </form>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                @endforeach
+                    @endforeach
+                </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 @endsection
