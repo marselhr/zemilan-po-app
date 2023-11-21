e46f18c9bb5caf1f3e8e24f2cc0db0793e646df1
Lukman <103345757+LukmanHakim2204@users.noreply.github.com>
2023-11-22 05:22:02 +0700

    add : view filtering product in catalog

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -39,8 +39,26 @@
     <section>
         <div class="container">
             <div class="row mt-4 justify-content-center">
-                <h2>Produk Favorit</h2>
-                <div class="d-flex flex-wrap col-10 justify-content-center">
+                <h2 class="text-center">Produk Favorit</h2>
+                <div class="col-10 col-md-6 filter mb-4 text-center">
+                    <form action="{{ route('filter.products') }}" method="get">
+                        <div class="row">
+                            <div class="col-md-8">
+                                <select name="category_filter" id="category_filter" class="form-select mb-3 mt-3">
+                                    <option value="" selected>Pilih Kategori</option>
+                                    @foreach ($categories as $category)
+                                        <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="col-md-4 mt-3">
+                                <button type="submit" class="btn btn-primary"><i class="fas fa-search"></i> Cari</button>
+                                <a href="{{ route('filter.products') }}" class="btn btn-dark"><i class="fas fa-arrow-rotate-left"></i> Reset</a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div id="productFilter" class="d-flex flex-wrap col-10 justify-content-center">
                     @foreach ($products as $product)
                         <!-- Medium-sized Card -->
                         <div href="{{ route('detail', ['id' => $product->id]) }}" class="col-10 col-md-5 col-lg-3 p-3 ">
