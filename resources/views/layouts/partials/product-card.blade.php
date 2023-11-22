5f7440929955f05398ddb5711a3ef82956e84f10
marsel945 <rewoherson519@gmail.com>
2023-11-22 17:45:09 +0800

    style product card

--- resources/views/layouts/partials/product-card.blade.php
+++ resources/views/layouts/partials/product-card.blade.php
@@ -22,16 +22,15 @@
                 <div class="d-flex flex-wrap gap-md-0 gap-1  col-12">
                     <div class="col-12 col-md-6 p-md-1">
                         <button type="button" data-product-id="{{ $product->id }}" data-quantity="1"
-                            class="btn btn-info w-100  add_to_cart" id="add_to_cart{{ $product->id }}">
-                            <i class="fe fe-shopping-cart text-white align-middle"></i>
+                            class="btn btn-outline-info w-100  add_to_cart" id="add_to_cart{{ $product->id }}">
+                            <i class="fe fe-shopping-cart align-middle"></i>
                             </a>
                     </div>
 
                     <div class="col-12 col-md-6 p-md-1">
                         <form action="{{ route('order.store', $product) }}" method="post">
                             @csrf
-                            <button type="submit" class="btn  btn-primary w-100"
-                                onclick="checkoutNow({{ $product->id }})">Beli
+                            <button type="submit" class="btn btn-primary w-100">Beli
                             </button>
                         </form>
                     </div>
