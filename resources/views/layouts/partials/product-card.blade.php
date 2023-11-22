08ad8e638233e163ff82413fd1b3849d4096cefc
marsel945 <rewoherson519@gmail.com>
2023-11-22 17:36:17 +0800

    fix (layout product card)

--- resources/views/layouts/partials/product-card.blade.php
+++ resources/views/layouts/partials/product-card.blade.php
@@ -12,17 +12,17 @@
                 <a href="#" class="fs-5"><i class="fe fe-heart align-middle"></i></a>
             </div>
             <a href="{{ route('detail', $product) }}">
-                <h4 class=" mb-0 text-truncate">{{ $product->name }}</h4>
+                <h4 class="mb-0 text-truncate">{{ $product->name }}</h4>
             </a>
+            <h6 class="mb-0 text-warning">Rp. {{ number_format($product->price, 0, '.', '.') }}</h6>
         </div>
         <!-- Card Footer -->
         <div class="card-footer">
             <div class="d-flex flex-wrap justify-content-between align-items-center">
-                <h5 class="mb-0">Rp. {{ $product->price }}</h5>
                 <div class="d-flex flex-wrap gap-md-0 gap-1  col-12">
                     <div class="col-12 col-md-6 p-md-1">
                         <button type="button" data-product-id="{{ $product->id }}" data-quantity="1"
-                            class="btn btn-primary w-100  add_to_cart" id="add_to_cart{{ $product->id }}">
+                            class="btn btn-info w-100  add_to_cart" id="add_to_cart{{ $product->id }}">
                             <i class="fe fe-shopping-cart text-white align-middle"></i>
                             </a>
                     </div>
@@ -30,7 +30,7 @@
                     <div class="col-12 col-md-6 p-md-1">
                         <form action="{{ route('order.store', $product) }}" method="post">
                             @csrf
-                            <button type="submit" class="btn  btn-success w-100"
+                            <button type="submit" class="btn  btn-primary w-100"
                                 onclick="checkoutNow({{ $product->id }})">Beli
                             </button>
                         </form>
