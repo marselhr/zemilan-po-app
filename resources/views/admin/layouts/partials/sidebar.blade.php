858a91f00b7b55160f894da4d3ba1db4e29c6a48
marsel945 <rewoherson519@gmail.com>
2023-11-22 23:07:39 +0800

    change link name

--- resources/views/admin/layouts/partials/sidebar.blade.php
+++ resources/views/admin/layouts/partials/sidebar.blade.php
@@ -40,10 +40,11 @@
                         class="nav-icon fe fe-gift me-2"></i>Manajemen Kupon</a>
             </li>
             <li class="nav-item">
-                <a href="{{route('admin.management.pesanan.index')}}" class="nav-link "> <i class="nav-icon fe fe-database me-2"></i>Manajemen Pesanan</a>
+                <a href="{{ route('admin.management.pesanan.index') }}" class="nav-link "> <i
+                        class="nav-icon fe fe-database me-2"></i>Manajemen Pesanan</a>
             </li>
             <li class="nav-item">
-                <a href="" class="nav-link "> <i class="nav-icon fe fe-pocket me-2"></i> Laporan Keuangan</a>
+                <a href="" class="nav-link "> <i class="nav-icon fe fe-pocket me-2"></i> Data Pemasukan</a>
             </li>
         </ul>
     </div>
