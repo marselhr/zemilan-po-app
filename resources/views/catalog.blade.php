afcf8bcf8e003be9a8178c07c76ae351b60de48e
marsel945 <rewoherson519@gmail.com>
2023-11-22 17:32:37 +0800

    refactor product card layout

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -32,45 +32,7 @@
                 <div id="productFilter" class="d-flex flex-wrap col-10 justify-content-center">
                     @foreach ($products as $product)
                         <!-- Medium-sized Card -->
-                        <div class="col-6 p-2 col-md-3 ">
-
-                            <div class="card">
-                                <img src="{{ $product->image != null ? asset('storage/' . $product->image) : 'https://source.unsplash.com/480x480?food' }}"
-                                    alt="" class="card-img-top">
-                                <!-- Card Body -->
-                                <div class="card-body">
-                                    <div class="d-flex justify-content-between align-items-center mb-3">
-                                        <a href="#" class="fs-5"><i class="fe fe-heart align-middle"></i></a>
-                                    </div>
-                                    <h4 class="mb-2 text-truncate">{{ $product->name }}</h4>
-                                    <small>Stock: {{ $product->stock }}</small>
-                                </div>
-                                <!-- Card Footer -->
-                                <div class="card-footer">
-                                    <div class="d-flex flex-wrap justify-content-between align-items-center">
-                                        <h5 class="mb-0">Rp. {{ $product->price }}</h5>
-                                        <div class="d-flex flex-wrap  col-12 mt-4">
-                                            <div class="col-md-6 col-12 p-1">
-                                                <button type="button" data-product-id="{{ $product->id }}"
-                                                    data-quantity="1" class="btn btn-primary w-100 add_to_cart"
-                                                    id="add_to_cart{{ $product->id }}">
-                                                    <i class="fe fe-shopping-cart text-white align-middle"></i>
-                                                    </a>
-                                            </div>
-
-                                            <div class="col-md-6 col-12 p-1">
-                                                <form action="{{ route('order.store', $product) }}" method="post">
-                                                    @csrf
-                                                    <button type="submit" class="btn  btn-success w-100"
-                                                        onclick="checkoutNow({{ $product->id }})">Beli
-                                                    </button>
-                                                </form>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        @include('layouts.partials.product-card')
                     @endforeach
                 </div>
             </div>
--- resources/views/layouts/partials/product-card.blade.php
+++ resources/views/layouts/partials/product-card.blade.php
@@ -1,28 +1,33 @@
-<div class="col-6 p-2 col-md-3 ">
+<div class="col-12   col-md-4  p-2">
 
     <div class="card">
         <img src="{{ $product->image != null ? asset('storage/' . $product->image) : 'https://source.unsplash.com/480x480?food' }}"
-            alt="" class="card-img-top">
+            alt="" class="card-img-top img-fluid"
+            style="idth: 100%;
+            height: 12vw;
+            object-fit: cover;">
         <!-- Card Body -->
         <div class="card-body">
-            <div class="d-flex justify-content-between align-items-center mb-3">
+            <div class="d-flex justify-content-between align-items-center mb-1">
                 <a href="#" class="fs-5"><i class="fe fe-heart align-middle"></i></a>
             </div>
-            <h4 class="mb-2 text-truncate">{{ $product->name }}</h4>
+            <a href="{{ route('detail', $product) }}">
+                <h4 class=" mb-0 text-truncate">{{ $product->name }}</h4>
+            </a>
         </div>
         <!-- Card Footer -->
         <div class="card-footer">
             <div class="d-flex flex-wrap justify-content-between align-items-center">
                 <h5 class="mb-0">Rp. {{ $product->price }}</h5>
-                <div class="d-flex flex-wrap  col-12 mt-4">
-                    <div class="col-md-6 col-12 p-1">
+                <div class="d-flex flex-wrap gap-md-0 gap-1  col-12">
+                    <div class="col-12 col-md-6 p-md-1">
                         <button type="button" data-product-id="{{ $product->id }}" data-quantity="1"
-                            class="btn btn-primary w-100 add_to_cart" id="add_to_cart{{ $product->id }}">
+                            class="btn btn-primary w-100  add_to_cart" id="add_to_cart{{ $product->id }}">
                             <i class="fe fe-shopping-cart text-white align-middle"></i>
                             </a>
                     </div>
 
-                    <div class="col-md-6 col-12 p-1">
+                    <div class="col-12 col-md-6 p-md-1">
                         <form action="{{ route('order.store', $product) }}" method="post">
                             @csrf
                             <button type="submit" class="btn  btn-success w-100"
