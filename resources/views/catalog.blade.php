e2f96fa9d9574397db58a3fea5b618cba67e9b3c
marsel945 <rewoherson519@gmail.com>
2023-11-22 16:22:59 +0800

    fix product card

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -53,7 +53,8 @@
                             </div>
                             <div class="col-md-4 mt-3">
                                 <button type="submit" class="btn btn-primary"><i class="fas fa-search"></i> Cari</button>
-                                <a href="{{ route('filter.products') }}" class="btn btn-dark"><i class="fas fa-arrow-rotate-left"></i> Reset</a>
+                                <a href="{{ route('filter.products') }}" class="btn btn-dark"><i
+                                        class="fas fa-arrow-rotate-left"></i> Reset</a>
                             </div>
                         </div>
                     </form>
@@ -61,46 +62,40 @@
                 <div id="productFilter" class="d-flex flex-wrap col-10 justify-content-center">
                     @foreach ($products as $product)
                         <!-- Medium-sized Card -->
-                        <div href="{{ route('detail', ['id' => $product->id]) }}" class="col-10 col-md-5 col-lg-3 p-3 ">
+                        <div class="col-6 p-2 col-md-3 ">
+
                             <div class="card">
-                                <a href="{{ route('detail', ['id' => $product->id]) }}">
-                                    <img src="{{ $product->image != null ? asset('storage/' . $product->image) : 'https://source.unsplash.com/480x480?food' }}"
-                                        alt="" class="card-img-top">
-                                    <!-- Card content goes here -->
-                                </a>
+                                <img src="{{ $product->image != null ? asset('storage/' . $product->image) : 'https://source.unsplash.com/480x480?food' }}"
+                                    alt="" class="card-img-top">
                                 <!-- Card Body -->
                                 <div class="card-body">
-                                    <div class="d-flex justify-content-between align-items-center mb-1">
+                                    <div class="d-flex justify-content-between align-items-center mb-3">
                                         <a href="#" class="fs-5"><i class="fe fe-heart align-middle"></i></a>
                                     </div>
-                                    <a href="{{ route('detail', ['id' => $product->id]) }}">
-                                        <h4 class="mb-2 text-truncate">{{ $product->name }}</h4>
-                                    </a>
+                                    <h4 class="mb-2 text-truncate">{{ $product->name }}</h4>
                                     <small>Stock: {{ $product->stock }}</small>
                                 </div>
                                 <!-- Card Footer -->
                                 <div class="card-footer">
                                     <div class="d-flex flex-wrap justify-content-between align-items-center">
                                         <h5 class="mb-0">Rp. {{ $product->price }}</h5>
-                                        <div class="d-flex gap-2">
-                                            <div>
+                                        <div class="d-flex flex-wrap  col-12 mt-4">
+                                            <div class="col-md-6 col-12 p-1">
                                                 <button type="button" data-product-id="{{ $product->id }}"
-                                                    data-quantity="1" class="btn btn-primary btn-sm add_to_cart"
+                                                    data-quantity="1" class="btn btn-primary w-100 add_to_cart"
                                                     id="add_to_cart{{ $product->id }}">
                                                     <i class="fe fe-shopping-cart text-white align-middle"></i>
-                                                </button>
+                                                    </a>
                                             </div>
 
-
-                                            <form action="{{ route('order.store', $product) }}" method="post">
-                                                <div>
+                                            <div class="col-md-6 col-12 p-1">
+                                                <form action="{{ route('order.store', $product) }}" method="post">
                                                     @csrf
-                                                    <button type="submit" class="btn btn-success btn-sm ml-1"
-                                                        onclick="checkoutNow({{ $product->id }})">
-                                                        Beli
+                                                    <button type="submit" class="btn  btn-success w-100"
+                                                        onclick="checkoutNow({{ $product->id }})">Beli
                                                     </button>
-                                                </div>
-                                            </form>
+                                                </form>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
