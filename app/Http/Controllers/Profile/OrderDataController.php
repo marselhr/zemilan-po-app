3b1df35034ddcd71050fa9483fb32d7cc552a9fb
marsel945 <rewoherson519@gmail.com>
2023-11-23 09:24:40 +0800

    feat(get data pesanan buyer)

---
+++ app/Http/Controllers/Profile/OrderDataController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Profile;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class OrderDataController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $data['orders'] = Auth::user()->orders;
+
+        $content = view('profile.order-data', compact('data'))->render();
+
+        return response()->json([
+            'status' => true,
+            'content' => $content,
+            'message' => 'Data berhasil diperoleh',
+        ]);
+    }
+}
