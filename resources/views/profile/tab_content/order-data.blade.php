916cb8e2c1781b729ff2efcdd43a3b2a080f6c17
Lukman <103345757+LukmanHakim2204@users.noreply.github.com>
2023-12-06 09:33:40 +0700

    Update : View History Pesanan In Buyer

--- resources/views/profile/tab_content/order-data.blade.php
+++ resources/views/profile/tab_content/order-data.blade.php
@@ -1,5 +1,27 @@
 <div class="col-xl-12 ">
-    @foreach ($data['orders'] as $order)
-        <p>{{ $order->id }} amount: {{ $order->gross_amount }}</p>
-    @endforeach
+    <h1 class="text-center">Riwayat Pesanan Anda</h1>
+    <div class="table-responsive border-0 overflow-y-hidden">
+        <table class="table mb-0 text-nowrap table-centered table-hover table-with-checkbox" id="datatable">
+            <thead class="table-light">
+                <tr>
+                    <th>No</th>
+                    <th>ID Pesanan</th>
+                    <th>Total Pembayaran</th>
+                    <th>Status Pembayaran</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                @foreach ($data['orders'] as $order)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $order->order_id }}</td>
+                        <td>Rp.{{ number_format($order->gross_amount, 0, '.', '.') }}</td>
+                        <td> <span class="badge bg-{{$order->payment_status == 'Paid' ? 'success':'danger'}}">
+                            {{ $order->payment_status }}</span></td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
